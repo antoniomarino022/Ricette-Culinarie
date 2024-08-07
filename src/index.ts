02ce5169203a8503aa3ae9978902768279619dbb
BuildTools <unconfigured@null.spigotmc.org>
2024-08-07 16:31:20 +0200

    methods

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express, {Request,Response} from 'express';
+
+
+const app = express(); // Crea un'applicazione Express
+const port =  "3000" // variabile di ambiente
+const baseURL ='http://localhost'
+const server = express.json(); // Middleware per parsare i dati JSON
+// const myApp = new Marketplace() // Crea un'istanza della classe Marketplace
+app.use(server);
