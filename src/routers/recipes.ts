90ec0f1f0619508c24044923f017fc3b668cccd8
sofia_liggio <sofialiggio994@gmail.com>
2024-08-11 14:26:43 +0200

    add user and recipe routers

--- src/routers/recipes.ts
+++ src/routers/recipes.ts
@@ -11,3 +11,36 @@ const client = createClient({
 });
 
 client.connect();
+
+// get recipes
+routerRecipe.get("", async (req: Request, res: Response) => {
+  const recipes = await client.query(`SELECT * FROM recipes`);
+  res.status(200).json(recipes.rows);
+});
+
+// add recipes
+routerRecipe.post("", (req: Request, res: Response) => {
+  const { name, ingredients } = req.body;
+
+  // Verifica che `name` e `ingredients` siano presenti
+  if (!name || !ingredients) {
+    return res
+      .status(400)
+      .json({ message: "Nome e ingredienti sono richiesti" });
+  }
+
+  // Query per inserire i dati nella tabella `recipes`
+  client.query(
+    `INSERT INTO recipes (name, ingredients) VALUES ($1, $2) RETURNING *`,
+    [name, ingredients],
+    (err, result) => {
+      if (err) {
+        return res.status(400).json({ error: err.message });
+      }
+      return res.status(201).json({
+        message: "Ricetta aggiunta con successo",
+        ricetta: result.rows[0],
+      });
+    }
+  );
+});
