ced304ff377daf15437137c4546d1e60b4aebc36
Gabriele Lo Re <gabrolr70@gmail.com>
2024-08-17 15:15:04 +0200

    added password verification

--- src/routers/user.ts
+++ src/routers/user.ts
@@ -47,35 +47,49 @@ routerUser.get("/:id", async (req: Request, res: Response) => {
 
 
 // update user
-routerUser.put("/:id", authenticateToken, async (req: Request, res: Response) => {
+routerUser.put("/:id", async (req: Request, res: Response) => {
   try {
     const idUser = req.params.id;
-    const { username, password } = req.body;
+    const { username, password, oldPassword } = req.body;
 
-    if (!username || !password || !idUser) {
+    if (!username || !password || !idUser || !oldPassword) {
       return res.status(400).json({ message: "Credenziali mancanti" });
     }
 
-    
+    const verify = await client.query("SELECT * FROM users WHERE username=$1", [
+      username
+    ]);
+
+    if (verify.rowCount === 0) {
+      return res.status(404).json({ message: "Utente non trovato" });
+    }
     
     const salt = await bcrypt.genSalt();
-    const passwordHash = await bcrypt.hash(password, salt);
-
-    const result = await client.query(
-      `UPDATE users
-       SET username = $1, 
-           password = $2
-       WHERE id = $3
-       RETURNING *`,
-      [username, passwordHash, idUser]
-    );
-
-    
-    if (result.rows.length > 0) {
-      return res.status(200).json({ message: "Credenziali aggiornate!", user: result.rows[0] });
-    } else {
-      return res.status(400).json({ message: "Credenziali non aggiornate!" });
+    const match = await bcrypt.compare(oldPassword, verify.rows[0].password);
+    if(match)
+    {
+      const passwordHash = await bcrypt.hash(password, salt);
+      const result = await client.query(
+        `UPDATE users
+         SET username = $1, 
+             password = $2
+         WHERE id = $3
+         RETURNING *`,
+        [username, passwordHash, idUser]
+      );
+  
+      
+      if (result.rowCount && result.rowCount > 0) {
+        return res.status(200).json({ message: "Credenziali aggiornate!", user: result.rows[0] });
+      } else {
+        return res.status(400).json({ message: "Credenziali non aggiornate!" });
+      }
     }
+    else
+    {
+      return res.status(401).json({ message: "Vecchia password sbagliata" });
+    }
+    
   } catch (error) {
     return res.status(500).json({ message: "Errore interno del server",error });
 
