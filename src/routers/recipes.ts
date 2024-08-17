593caa3bcf613dc090ec64d974bf37152ee97821
antoniomarino024@gmail.com <antoniomarino024@gmail.com>
2024-08-17 21:07:03 +0200

    fix api add,update and all get recipes

--- src/routers/recipes.ts
+++ src/routers/recipes.ts
@@ -13,69 +13,103 @@ const client = createClient({
 client.connect();
 
 // get recipes
-routerRecipe.get("", async (req: Request, res: Response) => {
-  const recipes = await client.query(`SELECT * FROM recipes`);
+routerRecipe.get("",authenticateToken, async (req: Request, res: Response) => {
+
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ 'message': 'Token mancante' });
+  }
+
+  const recipes = await client.query(`SELECT * FROM recipes `);
   res.status(200).json(recipes.rows);
 });
 
+
+
 // add recipes
-routerRecipe.post("/api/add/recipe",authenticateToken, (req: Request, res: Response) => {
-  const { name, ingredients } = req.body;
+routerRecipe.post("/add", authenticateToken, async (req: Request, res: Response) => {
+  try {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
 
-  // Verifica che `name` e `ingredients` siano presenti
-  if (!name || !ingredients) {
-    return res
-      .status(400)
-      .json({ message: "Nome e ingredienti sono richiesti" });
-  }
+    if (!token) {
+      return res.status(401).json({ 'message': 'Token mancante' });
+    }
 
-  // Query per inserire i dati nella tabella `recipes`
-  client.query(
-    `INSERT INTO recipes (name, ingredients) VALUES ($1, $2) RETURNING *`,
-    [name, ingredients],
-    (err, result) => {
-      if (err) {
-        return res.status(400).json({ error: err.message });
+    const { name, ingredients } = req.body;
+
+    if (!name || !ingredients) {
+      return res.status(400).json({ 'message': 'Nome e ingredienti sono richiesti' });
+    }
+
+    
+    const verify = await client.query('SELECT * FROM recipes WHERE name = $1 AND ingredients = $2', [name, ingredients]);
+
+    if (verify.rowCount && verify.rowCount > 0) {
+      return res.status(400).json({ 'message': 'Ricetta già esistente' });
+    } else {
+      const result = await client.query(
+        `INSERT INTO recipes (name, ingredients) VALUES ($1, $2) RETURNING *`,
+        [name, ingredients]
+      );
+  
+      if ( result.rowCount && result.rowCount > 0) {
+        return res.status(200).json({ 'message': 'Ricetta aggiunta con successo', 'recipe': result.rows[0] });
+      } else {
+        return res.status(400).json({ 'message': 'Ricetta non aggiunta' });
       }
-      return res.status(201).json({
-        message: "Ricetta aggiunta con successo",
-        ricetta: result.rows[0],
+    }
+
+  } catch (error) {
+    return res.status(500).json({ 'message': 'Errore del server',error });
+  }
+});
+
+// update recipe
+routerRecipe.put('/:idrecipe', authenticateToken, async (req: Request, res: Response) => {
+  try {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({ 'message': 'Token mancante' });
+    }
+    
+    const { idrecipe } = req.params; 
+    const { name, ingredients } = req.body;
+
+    if (!name || !ingredients) {
+      return res.status(400).json({ 'message': 'Nome e ingredienti sono richiesti' });
+    }
+
+    const verify = await client.query('SELECT * FROM recipes WHERE idrecipe = $1', [idrecipe]);
+
+    if (verify.rowCount === 0) {
+      return res.status(404).json({ 'message': 'Ricetta non esistente' });
+    } else {
+
+      const result = await client.query(
+        `UPDATE recipes SET name = $1, ingredients = $2 WHERE idrecipe = $3 RETURNING *`,
+        [name, ingredients, idrecipe]
+      );
+  
+      if (result.rowCount === 0) {
+        return res.status(404).json({ 'message': 'Ricetta non trovata' });
+      }
+  
+      return res.status(200).json({
+        'message': 'Ricetta aggiornata con successo',
+        'ricetta': result.rows[0],
       });
     }
-  );
-});
-
-// Update
-routerRecipe.put('/api/recipe/update/:idRecipe',authenticateToken, async (req: Request, res: Response) => {
-  const { idrecipe } = req.params; 
-  const { name, ingredients } = req.body;
-
-  if (!name || !ingredients) {
-    return res
-      .status(400)
-      .json({ message: "Nome e ingredienti sono richiesti" });
-  }
-
-  try {
     
-    const result = await client.query(
-      `UPDATE recipes SET name = $1, ingredients = $2 WHERE id = $3 RETURNING *`,
-      [name, ingredients, idrecipe]
-    );
-
-    if (result.rowCount === 0) {
-      return res.status(404).json({ message: "Ricetta non trovata" });
-    }
-
-    return res.status(200).json({
-      message: "Ricetta aggiornata con successo",
-      ricetta: result.rows[0],
-    });
   } catch (err) {
-    console.error('Errore nel database:',err);
-    return res.status(500).json({ error: 'Errore interno del server' });
+    return res.status(500).json({ 'error': 'Errore interno del server' });
   }
 });
+
 
 routerRecipe.delete('/api/recipe/delete:idRecipe',authenticateToken, async (req:Request,res:Response)=>{
   const { idrecipe } = req.params;
